f284ff978d39f9a38f8d6f88b7c99e5ef53349a6
Rafael Bellon <rafael.bellon@gmail.com>
2024-03-25 19:28:39 -0300

    Aula 6

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,10 @@
+var bodyParser = require("body-parser");
 module.exports = app => {
     app.set("port", 3000);
     app.set ("json spaces", 4);
+    app.use(bodyParser.json());
+    app.use((req, res, next) => {
+        delete req.body.id;
+        next();
+    });
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,62 @@
 module.exports = app => {  
   const Tasks = app.db.models.Tasks;
-  app.get("/tasks", (req, res) => {
-    Tasks.findAll({}).then(tasks => {  
-        res.json({tasks: tasks});
-    });
+  
+  app.route("/tasks")
+  //   .all((req, res, next) => {
+  //     delete req.body.id;
+  //     next();
+  // })
+    .get((req, res) => {
+      Tasks.findAll({})
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        })
+    // "/tasks": Lista tarefas
+  })
+    .post((req, res) => {
+      Tasks.create(req.body)
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        })
+    // "/tasks": Cadastra uma nova tarefa
+  });
+  
+  app.route("/tasks/:id")
+    .all((req, res, next) => {
+      delete req.body.id;
+      next();
+  })
+    .get((req, res) => {
+      Tasks.findOne({where: req.params})
+        .then(result => {
+          if (result) {
+            res.json(result);
+          } 
+          else {
+            res.sendStatus(404);
+          }
+        })
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    // "/tasks/1": Consulta uma tarefa
+  })
+    .put((req, res) => {
+      Tasks.update(req.body, {where: req.params})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    // "/tasks/1": Atualiza uma tarefa
+  })
+    .delete((req, res) => {
+      Tasks.destroy({where: req.params})
+          .then(result => res.sendStatus(204))
+          .catch(error => {
+            res.status(412).json({msg: error.message});
+          });
+    // "/tasks/1": Exclui uma tarefa
   });
 };
